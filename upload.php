5b1412e3b724a32e58cd83254fb270b84b8bbe25
Jerry Zou <zry656565@gmail.com>
2015-02-11 19:54:15 +0800

    fix bugs

--- upload.php
+++ upload.php
@@ -1,19 +1,19 @@
 <?php
 
-file_put_contents('/Users/Jerry/Dev/uploads/log', $current, LOCK_EX);
-function log($str) {
+file_put_contents('/Users/Jerry/Dev/uploads/log', '', LOCK_EX);
+function print_log($str) {
 	file_put_contents('/Users/Jerry/Dev/uploads/log', $str, FILE_APPEND | LOCK_EX);
 }
-log('[[$_FILES]]:' . print_r($_FILES, true));
-log('[[$_POST]]:' . print_r($_POST, true));
+print_log('[[$_FILES]]:' . print_r($_FILES, true));
+print_log('[[$_POST]]:' . print_r($_POST, true));
 
 if ($_POST['total'] === '1') {
 	$uploaddir = '/Users/Jerry/Dev/uploads/';
 	$uploadfile = $uploaddir . basename($_POST['name']);
 
 	if (move_uploaded_file($_FILES['fileData']['tmp_name'], $uploadfile)) {
-		log('success\n');
+		print_log('success\n');
 	} else {
-		log('failure\n');
+		print_log('failure\n');
 	}
 }
